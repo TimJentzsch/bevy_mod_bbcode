6128bfe17c419f09b774c47f8de9c71358afe4ef
Tim Jentzsch <commits@timjen.net>
2024-07-21 20:46:41 +0200

    Improve BBCode struct naming

--- src/bbcode/mod.rs
+++ src/bbcode/mod.rs
@@ -1,71 +1,70 @@
 use std::{collections::HashMap, fmt::Display, sync::Arc};
 
-pub enum BbcodeElement {
-    Node(BbcodeNode),
+pub enum BbcodeNode {
+    Tag(BbcodeTag),
     Text(String),
 }
 
-impl Display for BbcodeElement {
+impl Display for BbcodeNode {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            BbcodeElement::Node(node) => node.fmt(f),
-            BbcodeElement::Text(text) => text.fmt(f),
+            BbcodeNode::Tag(node) => node.fmt(f),
+            BbcodeNode::Text(text) => text.fmt(f),
         }
     }
 }
 
-pub struct BbcodeNode {
+pub struct BbcodeTag {
     /// The name of the tag, e.g. `tag` for `[tag]something[/tag]`.
-    tag: String,
+    name: String,
 
     /// A simple parameter for the tag, e.g. `value` for `[tag=value]something[/tag]`.
-    tag_param: Option<String>,
+    simple_param: Option<String>,
 
     /// Complex parameters, e.g. the map `value1` -> `xxx`, `value2` -> `yyy` for `[tag value1=”xxx” value2=”yyy”]something[/tag]`.
-    params: HashMap<String, String>,
+    complex_params: HashMap<String, String>,
 
     /// The child nodes (or text) contained inside this node.
-    children: Vec<Arc<BbcodeElement>>,
+    children: Vec<Arc<BbcodeNode>>,
 }
 
-impl BbcodeNode {
-    /// Create a new, empty node.
-    pub fn new<S: Into<String>>(tag: S) -> Self {
+impl BbcodeTag {
+    /// Create a new, empty tag.
+    pub fn new<S: Into<String>>(name: S) -> Self {
         Self {
-            tag: tag.into(),
-            tag_param: None,
-            params: HashMap::new(),
+            name: name.into(),
+            simple_param: None,
+            complex_params: HashMap::new(),
             children: Vec::new(),
         }
     }
 
-    /// Add a tag parameter.
-    pub fn with_tag_param<P: Into<String>>(mut self, tag_param: P) -> Self {
-        self.tag_param = Some(tag_param.into());
+    /// Add a simple parameter to the tag.
+    pub fn with_simple_param<P: Into<String>>(mut self, tag_param: P) -> Self {
+        self.simple_param = Some(tag_param.into());
         self
     }
 
     /// Add a key/value parameter.
     pub fn with_param<K: Into<String>, V: Into<String>>(mut self, key: K, value: V) -> Self {
-        self.params.insert(key.into(), value.into());
+        self.complex_params.insert(key.into(), value.into());
         self
     }
 
-    /// Add a nested node inside this one.
-    pub fn with_nested_node(mut self, node: BbcodeNode) -> Self {
-        self.children.push(Arc::new(BbcodeElement::Node(node)));
+    /// Add a nested tag inside this one.
+    pub fn with_tag(mut self, tag: BbcodeTag) -> Self {
+        self.children.push(Arc::new(BbcodeNode::Tag(tag)));
         self
     }
 
     /// Add text inside of the node.
     pub fn with_text<T: Into<String>>(mut self, text: T) -> Self {
-        self.children
-            .push(Arc::new(BbcodeElement::Text(text.into())));
+        self.children.push(Arc::new(BbcodeNode::Text(text.into())));
         self
     }
 }
 
-impl Display for BbcodeNode {
+impl Display for BbcodeTag {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         fn fmt_param(param: &str) -> String {
             if param.contains(' ') {
@@ -75,13 +74,13 @@ impl Display for BbcodeNode {
             }
         }
 
-        write!(f, "[{}", self.tag)?;
+        write!(f, "[{}", self.name)?;
 
-        if let Some(tag_param) = &self.tag_param {
+        if let Some(tag_param) = &self.simple_param {
             write!(f, "={}", fmt_param(tag_param))?;
         }
 
-        for (key, value) in &self.params {
+        for (key, value) in &self.complex_params {
             write!(f, " {key}={}", fmt_param(value))?;
         }
 
@@ -91,7 +90,7 @@ impl Display for BbcodeNode {
             child.fmt(f)?;
         }
 
-        write!(f, "[/{}]", self.tag)?;
+        write!(f, "[/{}]", self.name)?;
 
         Ok(())
     }
@@ -103,21 +102,21 @@ mod tests {
 
     #[test]
     fn display_bold() {
-        let node = BbcodeNode::new("b").with_text("test");
+        let node = BbcodeTag::new("b").with_text("test");
         assert_eq!(format!("{node}"), "[b]test[/b]");
     }
 
     #[test]
     fn display_color() {
-        let node = BbcodeNode::new("color")
-            .with_tag_param("#FF00FF")
+        let node = BbcodeTag::new("color")
+            .with_simple_param("#FF00FF")
             .with_text("test");
         assert_eq!(format!("{node}"), "[color=#FF00FF]test[/color]");
     }
 
     #[test]
     fn display_image() {
-        let node = BbcodeNode::new("img")
+        let node = BbcodeTag::new("img")
             .with_param("alt", "example image")
             .with_text("test");
         assert_eq!(format!("{node}"), r#"[img alt="example image"]test[/img]"#);
