7968614aaf33517c6df30666ea7a1e5eae97348e
Tim Jentzsch <commits@timjen.net>
2024-07-22 18:05:37 +0200

    Use new m tag for dynamic example

--- examples/dynamic.rs
+++ examples/dynamic.rs
@@ -1,8 +1,12 @@
-use bevy::prelude::*;
-use bevy_mod_bbcode::{Bbcode, BbcodeBundle, BbcodePlugin, BbcodeSettings};
+//! This example demonstrates how parts of the text can be efficiently updated dynamically.
+//! To do this, we use the special `[m]` tag, which allows us to assign a marker component to the contained text.
+//! We can then query for the marker component as usual and apply our edits.
 
-#[derive(Debug, Component)]
-struct Marker;
+use bevy::prelude::*;
+use bevy_mod_bbcode::{BbcodeBundle, BbcodePlugin, BbcodeSettings};
+
+#[derive(Component, Clone)]
+struct TimeMarker;
 
 fn main() {
     App::new()
@@ -15,27 +19,20 @@ fn main() {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
-    commands.spawn((
-        BbcodeBundle::from_content(
-            // The text will be added later
-            "",
-            BbcodeSettings::new(40., Color::WHITE)
-                .with_regular_font(asset_server.load("fonts/FiraSans-Regular.ttf"))
-                .with_bold_font(asset_server.load("fonts/FiraSans-Bold.ttf"))
-                .with_italic_font(asset_server.load("fonts/FiraSans-Italic.ttf")),
-        ),
-        Marker,
+    commands.spawn(BbcodeBundle::from_content(
+        "Time passed: [m=time]0.0[/m] s",
+        BbcodeSettings::new(40., Color::WHITE)
+            .with_regular_font(asset_server.load("fonts/FiraSans-Regular.ttf"))
+            .with_bold_font(asset_server.load("fonts/FiraSans-Bold.ttf"))
+            .with_italic_font(asset_server.load("fonts/FiraSans-Italic.ttf"))
+            // Register the marker component for the `m=time` tag
+            .with_marker("time", TimeMarker),
     ));
 }
 
-fn update(time: Res<Time>, mut query: Query<(&mut Bbcode, &mut BbcodeSettings), With<Marker>>) {
-    let (mut bbcode, mut settings) = query.single_mut();
-
-    // Dynamically change the text to the elapsed time
-    bbcode.content = format!(
-        "Time passed: [b][c=#ffffff]{:.2}[/c][/b]",
-        time.elapsed_seconds()
-    );
-    // Dynamically change the default text color
-    settings.color = Hsla::hsl((time.elapsed_seconds() * 20.) % 360., 1., 0.7).into();
+fn update(time: Res<Time>, mut query: Query<&mut Text, With<TimeMarker>>) {
+    for mut text in query.iter_mut() {
+        // We can directly query for the `Text` component and update it, without the BBCode being parsed again
+        text.sections[0].value = format!("{:.0}", time.elapsed_seconds());
+    }
 }
