ea44bb3b4be6d96d7898c4c092d05368768340ae
Tim Jentzsch <commits@timjen.net>
2024-07-26 13:18:42 +0200

    Improve structure of font module and exports

--- src/bevy/mod.rs
+++ src/bevy/mod.rs
@@ -2,3 +2,7 @@ pub(crate) mod bbcode;
 pub(crate) mod conversion;
 pub(crate) mod font;
 pub(crate) mod plugin;
+
+pub use bbcode::{Bbcode, BbcodeBundle, BbcodeSettings};
+pub use font::*;
+pub use plugin::BbcodePlugin;
