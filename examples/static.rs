ac8f57ba104f97fb2512d59a5bed04782446a24f
Tim Jentzsch <commits@timjen.net>
2024-07-28 12:24:49 +0200

    Expand static example with font family change

--- examples/static.rs
+++ examples/static.rs
@@ -13,7 +13,7 @@ fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 
     commands.spawn(BbcodeBundle::from_content(
-        "test [b]bold with [i]italic[/i][/b] and [c=#ff00ff]color[/c]",
+        r#"test [b]bold with [i]italic[/i][/b] and [c=#ff00ff]color[/c] and [font="JetBrains Mono"]different font[/font]"#,
         // Use the "Fira Sans" font family with a default font size of 40
         BbcodeSettings::new("Fira Sans", 40., Color::WHITE),
     ));
