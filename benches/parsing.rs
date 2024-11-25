a51b765dc4c4096269d5bcaaad0b0971e6fe1acd
Tim Jentzsch <commits@timjen.net>
2024-11-25 21:55:57 +0100

    Add additional benchmarks with larger input sizes

--- benches/parsing.rs
+++ benches/parsing.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use bevy_mod_bbcode::parser::parse_bbcode;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
@@ -11,5 +13,42 @@ pub fn small_raw_text(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, small_raw_text);
-criterion_main!(benches);
+pub fn small_text_with_simple_formatting(c: &mut Criterion) {
+    c.bench_function("small text with simple formatting", |b| {
+        b.iter(|| {
+            parse_bbcode(black_box(
+                "A simple text with [b]bold[/b] and [i]italic[i] elements",
+            ))
+        })
+    });
+}
+
+criterion_group!(
+    benches_small,
+    small_raw_text,
+    small_text_with_simple_formatting
+);
+
+pub fn large_raw_text(c: &mut Criterion) {
+    let input = &fs::read_to_string("benches/input/sample_5000_raw.bbcode").unwrap();
+
+    c.bench_function("large raw text", |b| {
+        b.iter(|| parse_bbcode(black_box(input)))
+    });
+}
+
+pub fn large_text_with_simple_formatting(c: &mut Criterion) {
+    let input = &fs::read_to_string("benches/input/sample_5000_simple.bbcode").unwrap();
+
+    c.bench_function("large text with simple formatting", |b| {
+        b.iter(|| parse_bbcode(black_box(input)))
+    });
+}
+
+criterion_group!(
+    benches_large,
+    large_raw_text,
+    large_text_with_simple_formatting
+);
+
+criterion_main!(benches_small, benches_large);
